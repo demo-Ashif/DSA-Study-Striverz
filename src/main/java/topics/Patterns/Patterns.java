f93c4bf14fba5d98ed5896611b4b9139dc93c861
Ashif Mujtoba <nmujtoba@gmail.com>
2023-11-03 18:34:55 +0600

    pattern7 solved

--- src/main/java/topics/Patterns/Patterns.java
+++ src/main/java/topics/Patterns/Patterns.java
@@ -3,7 +3,7 @@ package topics.Patterns;
 public class Patterns {
 
     public static void main(String[] args) {
-        Pattern1(3);
+        nStartTriangle(3);
     }
 
     static void Pattern1(int n) {
@@ -11,6 +11,77 @@ public class Patterns {
 
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void PatternNTriangle(int n) {
+        for (int row = 0; row < n; row++) {
+
+            for (int col = 0; col <= row; col++) {
+                System.out.print(col + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void PatternNTriangle2(int n) {
+        for (int row = 0; row < n; row++) {
+
+            for (int col = 0; col <= row; col++) {
+                System.out.print(row + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Seeding(int n) {
+        for (int i = n; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Seeding2(int n) {
+        for (int i = n; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print((j + 1) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void nStartTriangle(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            //space
+            for (int col = 1; col <= (n - (col + 1)); col++) {
+                System.out.print("2");
+            }
+
+            //star
+            for (int col = 1; col <= 2 * row - 1; col++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int col = 1; col <= (n - (col + 1)); col++) {
+                System.out.print("2");
             }
             System.out.println();
         }
