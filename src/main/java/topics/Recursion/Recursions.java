f103191ef9305ae69aa67dbffae1a66adaacd919
Ashif Mujtoba <ashif.mujtoba@mpower-social.com>
2023-11-20 17:31:17 +0600

    recursion problem solving

--- src/main/java/topics/Recursion/Recursions.java
+++ src/main/java/topics/Recursion/Recursions.java
@@ -7,29 +7,32 @@ import java.util.Arrays;
  */
 public class Recursions {
     public static void main(String[] args) {
-        printNos(50);
+//        printNos(50);
+        printNtimes(5);
     }
 
     public static  int[] printNos(int x) {
-
-        // Write Your Code Here
-
         int[] ans = new int[x];
-
         return printN(ans, x);
-
-//        System.out.print(Arrays.toString(ans));
     }
 
     static int[] printN(int[] ans, int n) {
-
         if (n > 1) {
             printN(ans, n - 1);
         }
-
         ans[n - 1] = n;
-
         return ans;
     }
 
+    public static void printNtimes(int n){
+        // Write your code here.
+        print(n);
+    }
+
+    static void print(int n){
+        if(n>1){
+            print(n-1);
+        }
+        System.out.print("Coding Ninjas ");
+    }
 }
