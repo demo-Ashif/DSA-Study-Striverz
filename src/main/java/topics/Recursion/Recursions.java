19374f5665de9b6f79f6028088a74e7803ce40ab
Ashif Mujtoba <nmujtoba@gmail.com>
2023-11-21 23:41:29 +0600

    recursion solving

--- src/main/java/topics/Recursion/Recursions.java
+++ src/main/java/topics/Recursion/Recursions.java
@@ -1,16 +1,40 @@
 package topics.Recursion;
 
-import java.util.Arrays;
-
 /*
  * https://leetcode.com/problems/plus-one/
  */
 public class Recursions {
     public static void main(String[] args) {
-        printNos(50);
+//        printNos(50);
+//        System.out.print(sumFirstN(3));
+//        printNtoOne(1, 5);
+        System.out.print(printFactorial(31));
     }
 
-    public static  int[] printNos(int x) {
+
+    public static long sumFirstN(long n) {
+        // Write your code here.
+        return (n * (n + 1)) / 2;
+    }
+
+    // print N to 1 without f(i-1,N)
+
+    public static void printNtoOne(int i, int n) {
+        if (i < n) {
+            printNtoOne(i + 1, n);
+        }
+
+        System.out.print(i);
+    }
+
+    public static int printFactorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * printFactorial(n - 1);
+    }
+
+    public static int[] printNos(int x) {
 
         // Write Your Code Here
 
@@ -18,7 +42,7 @@ public class Recursions {
 
         return printN(ans, x);
 
-//        System.out.print(Arrays.toString(ans));
+        //System.out.print(Arrays.toString(ans));
     }
 
     static int[] printN(int[] ans, int n) {
