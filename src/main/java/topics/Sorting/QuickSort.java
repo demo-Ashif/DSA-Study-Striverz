0a339a1a2097bc78eaf27cdafc2a3ee26057f114
Ashif Mujtoba <ashif.mujtoba@mpower-social.com>
2023-11-30 18:58:36 +0600

    commit before doing quicksort partition index

---
+++ src/main/java/topics/Sorting/QuickSort.java
@@ -0,0 +1,30 @@
+package topics.Sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] data = {4, 6, 2, 5, 7, 9, 1, 3};
+
+
+        for (int datum : data) {
+            System.out.print(datum + " ");
+        }
+    }
+
+    static void makePartition(int[] arr, int pivot) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        int i = low;
+        int j = high;
+
+        for(i=1;i<=high;i++){
+            if(arr[i])
+        }
+
+
+    }
+
+}
