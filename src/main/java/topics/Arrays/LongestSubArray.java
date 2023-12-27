25e3054534db307391f61209e7bbf29101d43768
Ashif Mujtoba <ashif.mujtoba@mpower-social.com>
2023-12-27 18:27:05 +0600

    solved two pointer approach for longest subarray

--- src/main/java/topics/Arrays/LongestSubArray.java
+++ src/main/java/topics/Arrays/LongestSubArray.java
@@ -7,14 +7,15 @@ public class LongestSubArray {
 
     public static void main(String[] args) {
 //        int[] arr = new int[]{1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-        int[] arr = new int[]{2, 0, 0, 3};
-        subArray(arr);
+        int[] arr = new int[]{1, 2, 1, 3};
+//        subArray(arr);
+        subArrayTwoPointer(arr, 2);
     }
 
     static void subArray(int[] nums) {
         int maxLen = 0;
-        int k = 3;
         long sum = 0;
+        long k = 3;
 
         Map<Long, Integer> preSumMap = new HashMap<>();
 
@@ -38,7 +39,38 @@ public class LongestSubArray {
             }
         }
 
+//        return maxLen;
 
         System.out.println(maxLen);
+    }
+
+    static int subArrayTwoPointer(int[] nums, long k) {
+        if (nums.length <= 1) return 1;
+
+        int maxLen = 0;
+        long sum = nums[0];
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length) {
+            while (left <= right && sum > k) {
+                sum = sum - nums[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            right++;
+            if (right < nums.length) {
+                sum += nums[right];
+            }
+        }
+
+        System.out.println(maxLen);
+
+        return maxLen;
+
     }
 }
