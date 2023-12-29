0fd7a002028a2004a8b385909a7cfa32682ffdb3
Ashif Mujtoba <nmujtoba@gmail.com>
2023-12-30 00:12:30 +0600

    solved tow sum again

--- src/main/java/topics/Arrays/TwoSum.java
+++ src/main/java/topics/Arrays/TwoSum.java
@@ -5,17 +5,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
-* https://leetcode.com/problems/two-sum/
-*/
+ * https://leetcode.com/problems/two-sum/
+ */
 public class TwoSum {
     public static void main(String[] args) {
-        int[] result = twoSum(new int[]{2, 7, 11, 15},9);
+        int[] result = twoSumTwoPointer(new int[]{3,2,4}, 6);
         System.out.print(Arrays.toString(result));
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumMap(int[] nums, int target) {
         int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 result[1] = i;
@@ -27,4 +27,28 @@ public class TwoSum {
         }
         return result;
     }
+
+    static int[] twoSumTwoPointer(int[] numbers, int target) {
+
+        //considering inout array is sorted
+        Arrays.sort(numbers);
+        int start = 0;
+        int end = numbers.length - 1;
+        int[] result = new int[2];
+
+        while (start < end) {
+            int value = numbers[start] + numbers[end];
+            if (value == target) {
+                result[0] = start;
+                result[1] = end;
+                break;
+            } else if (value < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return result;
+    }
+
 }
