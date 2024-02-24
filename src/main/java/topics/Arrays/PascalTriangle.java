7236ed962345ae1f73dfc1baa48f2b55f1a682c8
Ashif Mujtoba <nmujtoba@gmail.com>
2024-02-24 11:36:10 +0600

    pascal triangle done again

--- src/main/java/topics/Arrays/PascalTriangle.java
+++ src/main/java/topics/Arrays/PascalTriangle.java
@@ -9,30 +9,31 @@ import java.util.List;
  */
 public class PascalTriangle {
     public static void main(String[] args) {
-        List<List<Integer>> result = generate(5);
+        List<List<Integer>> result = generatePascal(5);
         System.out.print(Arrays.toString(result.toArray()));
     }
 
-    static public List<List<Integer>> generate(int numRows) {
+    static public List<List<Integer>> generatePascal(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
         if (numRows == 0) return triangle;
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
+
+        // Add the first row [1] to the triangle
+        triangle.add(new ArrayList<>(List.of(1)));
 
         for (int i = 1; i < numRows; i++) {
-            List<Integer> curr = new ArrayList<>();
-            curr.add(1);
+            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> row = new ArrayList<>();
 
+            row.add(1); // First element of each row is 1
+
+            // Calculate middle elements of the row
             for (int j = 1; j < i; j++) {
-                //measuring how elements need to be inserted as per pascal triangle
-                // and inserting those value by adding them from i-1 list
-                int element = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
-                curr.add(j, element);
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
-            curr.add(1);
-            triangle.add(curr);
-        }
 
+            row.add(1); // Last element of each row is 1
+            triangle.add(row);
+        }
 
         return triangle;
     }
